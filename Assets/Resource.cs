16d5d8c358231acdeaa77d096bfcc59d6c14814d
Keane <KeaneCarotenuto@gmail.com>
2021-09-08 15:46:17 +1200

    Simple Resources

---
+++ Assets/Resource.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Resource : MonoBehaviour
+{
+    public enum Type
+    {
+        Organic,
+        Power,
+        Scrap
+    }
+
+    [SerializeField] GameObject m_player;
+    [SerializeField] Canvas m_canvas;
+    [SerializeField] TextMeshProUGUI m_pickupText;
+
+    [SerializeField] Type m_type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!m_player) m_player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_canvas.transform.rotation = Quaternion.identity;
+        m_canvas.transform.position = transform.position + Vector3.up;
+
+        if (Vector2.Distance(m_player.transform.position, transform.position) < 3)
+        {
+            m_pickupText.enabled = true;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                m_player.GetComponent<Inventory>().AddResource(m_type);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            m_pickupText.enabled = false;
+        }
+    }
+
+    public Type GetResourceType()
+    {
+        return m_type;
+    }
+}
---
+++ Assets/Scripts/EnemyTest.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    [System.Serializable]
+    public class Drop
+    {
+        public GameObject m_prefab;
+        public float m_amount;
+    }
+
+    [SerializeField] float m_health = 100.0f;
+    bool m_alive = true;
+
+
+    [SerializeField] List<Drop> m_drops;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -10)
+        {
+            Revive();
+        }
+    }
+
+    public void TakeDamage(float _amount)
+    {
+        m_health -= _amount;
+
+        if (m_alive && m_health <= 0)
+        {
+            Die();
+        }
+
+        UpdateVisuals();
+    }
+
+    private void Revive()
+    {
+        m_alive = true;
+        m_health = 100;
+        transform.position = new Vector3(4, 5, 0);
+        transform.rotation = Quaternion.identity;
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+        GetComponent<Rigidbody2D>().mass *= 2.0f;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        UpdateVisuals();
+    }
+
+    public void Die()
+    {
+        m_alive = false;
+        m_health = 0;
+        GetComponent<Rigidbody2D>().freezeRotation = false;
+        GetComponent<Rigidbody2D>().mass *= 0.5f;
+
+        foreach (Drop _drop in m_drops)
+        {
+            for (int i = 0; i < _drop.m_amount; i++)
+            {
+                GameObject newDrop = Instantiate(_drop.m_prefab, transform.position + transform.up, Quaternion.identity, null);
+            }
+        }
+    }
+
+    public void UpdateVisuals()
+    {
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        rend.color = new Color(1, m_health / 100, m_health / 100);
+    }
+}
