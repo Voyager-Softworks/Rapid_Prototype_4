9a701b593b100dcd1ad80855f57f8981db64688d
Keane <KeaneCarotenuto@gmail.com>
2021-11-03 17:28:01 +1300

    Working on bounty Board

---
+++ Assets/BountyBoard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BountyBoard : MonoBehaviour
+{
+    [Header("Board")]
+    public GameObject bountyBoard;
+    public GameObject canvas;
+
+    [Header("Main")]
+    public GameObject mainPannel;
+    public GameObject mainTitle;
+
+    [Header("Info")]
+    public GameObject infoPannel;
+    public GameObject infoTitle;
+
+    [Header("Difficulty")]
+    public GameObject difficulty;
+    public GameObject difficultyDecrease;
+    public GameObject difficultyIncrease;
+
+    [Header("Missions")]
+    public GameObject mission1;
+    public GameObject mission2;
+    public GameObject mission3;
+}
