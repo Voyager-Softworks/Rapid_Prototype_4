01e19a177c725800bdab2e1a722b5ac233bd55d0
Keane <KeaneCarotenuto@gmail.com>
2021-11-24 11:15:49 +1300

    Pause Menu works

---
+++ Assets/EscapeMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EscapeMenu : MonoBehaviour
+{
+    public GameObject escapeMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            escapeMenu.SetActive(!escapeMenu.activeSelf);
+            Time.timeScale = (escapeMenu.activeSelf) ? 0 : 1;
+        }
+    }
+}
